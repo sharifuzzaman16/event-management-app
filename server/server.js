8fc248240d774fbb45eb02e8b4a828e4e93709a4
sharifuzzaman16 <private.sharifuzzaman@gmail.com>
2025-06-29 23:06:58 +0600

    Ready for Railway deployment

--- server/server.js
+++ server/server.js
@@ -2,7 +2,8 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
+
 
 const { connectToDb } = require('./db');
 

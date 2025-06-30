1e7f051185966f30237abcfc047618b86cf7f667
sharifuzzaman16 <private.sharifuzzaman@gmail.com>
2025-06-30 23:39:06 +0600

    imported path

--- server/server.js
+++ server/server.js
@@ -1,34 +1,29 @@
-require('dotenv').config();
-const express = require('express');
-const cors = require('cors');
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const path = require("path");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+const { connectToDb } = require("./db");
 
-const { connectToDb } = require('./db');
-
-const authRoutes = require('./routes/auth');
-const eventsRoute = require('./routes/events');
+const authRoutes = require("./routes/auth");
+const eventsRoute = require("./routes/events");
 
 app.use(cors());
 app.use(express.json());
 
-app.use('/api/auth', authRoutes);
-app.use('/api/events', eventsRoute);
-
-
-app.get('/', (req, res) => {
-  res.send('Hello from the Event Management backend!');
-});
+app.use("/api/auth", authRoutes);
+app.use("/api/events", eventsRoute);
 
 app.use(express.static(path.join(__dirname, "build")));
+
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "build", "index.html"));
 });
 
-
 connectToDb().then(() => {
   app.listen(PORT, () => {
-    // console.log(`Server is running on port ${PORT}`);
+    console.log(`Server is running on port ${PORT}`);
   });
 });

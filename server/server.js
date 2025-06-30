c87d9e0d9256c114a44cfc4ac8e2c9eec98a0670
sharifuzzaman16 <private.sharifuzzaman@gmail.com>
2025-06-30 23:42:52 +0600

    fixed the data not loading issue

--- server/server.js
+++ server/server.js
@@ -1,29 +1,29 @@
-require("dotenv").config();
-const express = require("express");
-const cors = require("cors");
-const path = require("path");
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-const { connectToDb } = require("./db");
 
-const authRoutes = require("./routes/auth");
-const eventsRoute = require("./routes/events");
+const { connectToDb } = require('./db');
+
+const authRoutes = require('./routes/auth');
+const eventsRoute = require('./routes/events');
 
 app.use(cors());
 app.use(express.json());
 
-app.use("/api/auth", authRoutes);
-app.use("/api/events", eventsRoute);
+app.use('/api/auth', authRoutes);
+app.use('/api/events', eventsRoute);
 
-app.use(express.static(path.join(__dirname, "build")));
 
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "build", "index.html"));
+app.get('/', (req, res) => {
+  res.send('Hello from the Event Management backend!');
 });
+
 
 connectToDb().then(() => {
   app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+    // console.log(`Server is running on port ${PORT}`);
   });
 });
